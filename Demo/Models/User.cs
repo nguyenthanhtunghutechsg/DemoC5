50dce354fd5ce8bdce2cea1912094b35f3b3b1e9
Tungnt <nguyenthanhtunghutechsg@gmail.com>
2023-12-21 13:00:11 +0700

    Create file User.cs

---
+++ Demo/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Demo.Models
+{
+    public class User
+    {
+		private int iD;
+
+		public int ID
+		{
+			get { return iD; }
+			set { iD = value; }
+		}
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+
+
+	}
+}
